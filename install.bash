f897f726c4b8f90197625cb6be838bfecca2df0a
javierorozco2 <eorozco8@ucol.mx>
2020-03-23 02:24:45 -0600

    version 0.3

---
+++ install.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo pip install ez_setup
+sudo pip install moviepy
---
+++ prueba.py
@@ -0,0 +1,31 @@
+import pygame, sys
+from moviepy.editor import VideoFileClip
+from pygame.locals import *
+
+pygame.init()
+
+reloj = pygame.time.Clock()
+x=480
+y=320
+fps=60
+def video():
+    pygame.init()
+    pygame.display.set_caption("Swoosh Console")
+    video = VideoFileClip("Animacion.mp4")
+    video.preview()
+    main()
+def main():
+    pygame.init()
+
+    pygame.display.set_caption("Swoosh Console")
+    screen = pygame.display.set_mode((x,y))
+
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit(0)
+        pygame.display.update()
+        reloj.tick(fps)
+video()
