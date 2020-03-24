f2ef1ab5f70581e33dd55bb40a214cbfee8b4477
javierorozco2 <eorozco8@ucol.mx>
2020-03-24 14:23:13 -0600

    actualizacion 24-03-20-2:22

--- init.py
+++ init.py
@@ -9,15 +9,10 @@ x=480
 y=320
 fps=60
 
-
-def musica():
-    pygame.mixer.music.load("MusicaAmbiente1.mp3")
-    pygame.mixer.music.play(1)
-
 def video():
     pygame.init()
     pygame.display.set_caption("Swoosh Console")
-    video = VideoFileClip("Animacion.mp4")
+    video = VideoFileClip("movie/Intro.mp4")
     video.preview()
     main()
 def main():
@@ -25,14 +20,14 @@ def main():
 
     pygame.display.set_caption("Swoosh Console")
     screen = pygame.display.set_mode((x,y))
-
+    fondo = pygame.image.load("images/Mainmenu.png")    
     
     while True:
+        screen.blit(fondo,(0,0))
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit(0)
         pygame.display.update()
         reloj.tick(fps)
-musica()
 video()
--- install.bash
+++ install.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+sudo apt-get install python-pygame
 sudo pip install ez_setup
 sudo pip install moviepy
