491d143705fc1cca5fd085337cd6a0793069976a
javierorozco2 <eorozco8@ucol.mx>
2020-04-10 04:29:08 -0500

    v0.27

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from wifi import Cell
 from wireless import Wireless
 from pygame_vkeyboard import *
 
-
 pygame.init()
 
 reloj = pygame.time.Clock()
@@ -92,6 +91,8 @@ def main():
                     #teclado os.system("") 
                 if cursor1.colliderect(botonajustes.rect):
                     ajustes()
+                if cursor1.colliderect(botonjuegos.rect):
+                	juegos()
                     
         cursor1.update()
         botonjuegos.update(screen,cursor1)
@@ -100,6 +101,53 @@ def main():
         botonsalir.update(screen,cursor1)
         pygame.display.update()
         reloj.tick(fps)
+
+def juegos():
+
+    pygame.init()
+    #---------------Display---------------------
+    screen = pygame.display.set_mode((480,320))
+    pygame.display.set_caption("Swoosh console")
+    #--------------Imagenes---------------------
+    barra = pygame.image.load("images/menujuegos/barra.png")
+    fondo = pygame.image.load("images/Mainmenu.png")
+    #-------Variables y objetos----------------
+    crono=(pygame.time.get_ticks()/1000)
+    seleccion = seleccionmj(0,2)
+    imagenes = presentar(0,0,crono)
+    #--BUCLE--
+    while True:
+    	screen.blit(fondo,(0,0))
+    	imagenes.update(screen,seleccion.ax)
+    	#---------Evento cerrar ventana-----
+    	for event in pygame.event.get():
+    		if event.type == QUIT:
+    			pygame.quit()
+    			sys.exit(0)
+    		#-----EVENTOS DE TECLADO---------
+    		if event.type == pygame.KEYDOWN:
+    			#---------SUBIR Y BAJAR---------
+    			if event.key == K_DOWN:
+    				seleccion.ax+=1
+    			if event.key == K_UP:
+    				seleccion.ax-=1
+    			#---MOVIMIENTO ENTRE OPCIONES---
+    			if event.key == K_LEFT:
+    				seleccion.boolatras=True
+    				seleccion.boolplay=False
+    			if event.key == K_RIGHT:
+    				seleccion.boolplay=True
+    				seleccion.boolatras=False
+    			#------SELECCION DE OPCION------
+    			if event.key == K_RETURN:
+    				seleccion.opciones()
+
+    	imagenes.mover(crono)
+    	seleccion.acciones()
+    	screen.blit(barra,(0,0))
+    	seleccion.update(screen)
+    	reloj.tick(60)
+    	pygame.display.update()
 
 def ajustes():
     pygame.init()
@@ -403,9 +451,7 @@ class selecwifi(pygame.sprite.Sprite):
         if self.boolatras==True:
             screen.blit(self.atras,(10,150))
         else:
-<<<<<<< HEAD
             screen.blit(self.imagen,(self.rect.x,self.rect.y))       
-=======
             screen.blit(self.imagen,(self.rect.x,self.rect.y))
     
 #///////////////////////////////////////////////////////////////////////////////    
@@ -481,14 +527,137 @@ def wififinal(ssid):
                         running = False
                         ciclo = False
                         keyboard.disable()
+            	if event.type==QUIT:
+            		pygame.quit(0)
+            		sys.exit()
                        
                 keyboard.on_event(event)
                 pygame.display.update()
                 reloj.tick(fps)          
-                
-#///////////////////////////////////////////////////////////////////////////////       
->>>>>>> refs/remotes/origin/master
+class seleccionmj(pygame.sprite.Sprite):
+	def __init__(self,x,y):
+		pygame.sprite.Sprite.__init__(self)
+		#------------------Imagenes---------------------
+		self.imagen = pygame.image.load("images/menujuegos/seleccion.png")
+		self.play = pygame.image.load("images/menujuegos/play.png")
+		self.atras1 = pygame.image.load("images/menujuegos/atras1.png")
+		self.atras2 = pygame.image.load("images/menujuegos/atras2.png")
+		self.miniatura= pygame.image.load("images/menujuegos/eq6logo.png")
+		#-----------------Rectangulo--------------------
+		self.rect = self.imagen.get_rect() #SELECCION
+		self.rect.x = x
+		self.rect.y = y
 
+		self.atrasrect = self.atras1.get_rect()#ATRAS
+		self.playrect = self.play.get_rect()#PLAY
+		self.miniaturarect = self.miniatura.get_rect()#MINIATURA
+		#---------------- Variables---------------------
+		self.y = y
+		self.ax= 0
+		self.boolplay=False
+		self.boolatras=False
+	def acciones(self):
+		#-------------ABAJO-----------------
+		if self.ax==1 and self.rect.y<=45:
+			self.rect.y+=5
+		if self.ax==2 and self.rect.y<=90:
+			self.rect.y+=5
+		if self.ax==3 and self.rect.y<=135:
+			self.rect.y+=5
+		if self.ax==4 and self.rect.y<=180:
+			self.rect.y+=5
+		if self.ax==5 and self.rect.y<=225:
+			self.rect.y+=5
+		if self.ax==6 and self.rect.y<=270:
+			self.rect.y+=5
+		if self.ax==7:
+			self.ax=0
+			self.rect.y=self.y
+		#-----------ARRIBA-----------------
+		if self.ax==5 and self.rect.y>230:
+			self.rect.y-=5
+		if self.ax==4 and self.rect.y>185:
+			self.rect.y-=5
+		if self.ax==3 and self.rect.y>140:
+			self.rect.y-=5
+		if self.ax==2 and self.rect.y>95:
+			self.rect.y-=5
+		if self.ax==1 and self.rect.y>50:
+			self.rect.y-=5
+		if self.ax==0 and self.rect.y>5:
+			self.rect.y-=5
+		if self.ax<0:
+			self.ax=6
+			self.rect.y=270
+		#-------MOVIMIENTO DE BOTONES-----
+		self.atrasrect.y=self.rect.y+13
+		self.playrect.y = self.rect.y+8
+		self.miniaturarect.y = self.rect.y+5
+		#------DEFINICION DE NIVELES-----
+		self.niveles()
+	def opciones(self):
+		pass
+		'''
+		if self.boolplay==True:
+			self.nivel()
+		'''
+		if self.boolatras==True:
+			main()
+
+	def niveles(self):
+		'''
+		Aqui falta agregar la miniatura de los demas juegos
+		'''
+		if self.ax==0:
+			#Plumber Jumper
+			self.miniatura= pygame.image.load("images/menujuegos/eq6logo.png")
+		if self.ax==1:
+			#Push-planet
+			self.miniatura= pygame.image.load("images/menujuegos/eq4logo.png")
+		else:
+			self.miniatura= pygame.image.load("images/menujuegos/eq6logo.png")
+		self.miniatura
+	def update(self,screen):
+		#---Imagen seleccion---
+		screen.blit(self.imagen,(self.rect.x,self.rect.y))
+		#--Imagen boton atras--
+		screen.blit(self.atras1,(5,self.atrasrect.y))
+		#---Imagen miniatura---
+		screen.blit(self.miniatura,(33,self.miniaturarect.y))
+		if self.boolatras==True:
+			#----Imagen atras en seleccion--
+			screen.blit(self.atras2,(5,self.atrasrect.y))
+		if self.boolplay==True:
+			#-----Imagen play en seleccion--
+			screen.blit(self.play,(130,self.playrect.y))
+
+class presentar(pygame.sprite.Sprite):
+	def __init__(self,x,y,crono2):
+		pygame.sprite.Sprite.__init__(self)
+		#Plumber jumper
+		self.imagen = pygame.image.load("images/capturas/panoramica.png")
+		#Push planet
+		self.imagen2 = pygame.image.load("images/capturas/panoramica2.png")
+		self.rect = self.imagen.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.crono2=crono2
+	def mover(self,crono2):
+		self.crono = (pygame.time.get_ticks()/1000)-self.crono2
+		if self.crono>=3 and self.rect.x>(-480):
+			self.rect.x-=5
+		if self.crono>=8 and self.rect.x>(-960):
+			self.rect.x-=5
+
+		if self.crono == 13:
+			self.crono2+=13
+			self.rect.x=0
+
+	def update(self,screen,ax):
+		if ax==0:
+			screen.blit(self.imagen,(self.rect.x,self.rect.y))
+		elif ax==1:
+			screen.blit(self.imagen2,(self.rect.x,self.rect.y))
 #-----------LLAMADO DE CLASE PRINCIPAL----------
-if __name__ == "__main__":
-    video()
+main()
+#video()
